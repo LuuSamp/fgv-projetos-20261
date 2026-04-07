72c0071efad5d361f0c301a3d34883b71442a9ae
KaikyBraga <kaiky.braga06221607@gmail.com>
2026-04-06 21:32:59 -0300

    fix: file names

--- assignment_1/task_1/grupo_4/final/main.py
+++ assignment_1/task_1/grupo_4/final/main.py
@@ -1,7 +1,7 @@
-from scripts.01_provision_rds import main as provision_rds
-from scripts.02_load_classicmodels import main as load_classicmodels
-from scripts.03_validate_classicmodels import main as validate_classicmodels
-from scripts.04_destroy_rds import main as destroy_rds
+from scripts.a_provision_rds import main as provision_rds
+from scripts.b_load_classicmodels import main as load_classicmodels
+from scripts.c_validate_classicmodels import main as validate_classicmodels
+from scripts.d_destroy_rds import main as destroy_rds
 
 if __name__ == "__main__":
     provision_rds()
